14f5b5e2d1aaf4b20ceeb721732c37e196936848
DESKTOP-9B4UMLR\Annis <l536510961@126.com>
2018-10-16 16:01:22 +0800

    Initial commit

---
+++ app/src/main/java/com/cmct/mvvmlearn/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cmct.mvvmlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment);
+        return NavHostFragment.findNavController(fragment).navigateUp();
+    }
+}
